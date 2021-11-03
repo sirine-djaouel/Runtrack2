c44aa94a85ec9de3e4e1a761d259cfcc3dac1d35
Sirine <sirine.djaouel@laplateforme.io>
2021-11-03 19:44:42 +0100

    Jour 2
    job05 terminé // jour 2 terminé

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -3,21 +3,23 @@
 //Les nombres premiers sont des entiers naturels divisable uniquement par eux-même ou par 2
 // ils ont donc 2 diviseurs : 1 et eux-même
 
-for($i=1; $i<=1000; $i++){
-
+for($i=2; $i<=1000; $i++){
+ 
 
     for($j=1; $j<=$i; $j++){
-        if($i%$j==0){00
-          
+        $x=0;
 
+        if($i%$j==0){
+            $x++;
         }
+           
+       
     }
 
-    
-}
-
-
-
+        if($x==2){
+            echo $i."<br>";
+        }
+        
 
 }
 
