d335f72bec9530beebe27ba959d6f6046444505b
Sirine <sirine.djaouel@laplateforme.io>
2021-11-03 19:45:40 +0100

    Update index.php

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,26 +1,20 @@
 <?php
 
-//Les nombres premiers sont des entiers naturels divisable uniquement par eux-même ou par 2
-// ils ont donc 2 diviseurs : 1 et eux-même
-
 for($i=2; $i<=1000; $i++){
- 
+    $x=0;
 
     for($j=1; $j<=$i; $j++){
-        $x=0;
+        
 
         if($i%$j==0){
             $x++;
         }
-           
-       
     }
 
-        if($x==2){
-            echo $i."<br>";
-        }
-        
-
+    if($x==2){
+        echo $i."<br>";
+    }
 }
+
 
 ?>
