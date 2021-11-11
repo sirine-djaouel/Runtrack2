9eb98f5a22f0099dd33aa3ff4d6423bc7ed435ab
Sirine <sirine.djaouel@laplateforme.io>
2021-11-11 22:00:36 +0100

    Commit jour4  et consignes

---
+++ jour04/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+var_dump($_GET);
+
+?>
+
+
+<form action="./index.php" method="get">
+<label for="fname">First Name:</label>
+<input type="text" id=fname name=fname>
+<label for="lname">Last Name:</label>
+<input type="text" id=lname name=lname>
+<input type=submit value =submit>
+<p><input type="submit" value="Soummettre"></p>
+</form>
