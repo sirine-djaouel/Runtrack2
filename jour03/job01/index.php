2726092a6ce3e9a6a3f789f9bdfca1e031138e14
Sirine <sirine.djaouel@laplateforme.io>
2021-11-12 11:28:22 +0100

    Update index.php

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -12,7 +12,7 @@ $i=0;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Runtrack2 Jour 2 - Job1</title>
+    <title>Runtrack2 Jour 3 - Job1</title>
 </head>
 <table>
     <tbody>
