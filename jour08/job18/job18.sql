13060d3c1c0a62a01a6b09090c8962df78337ce5
Sirine <sirine.djaouel@laplateforme.io>
2021-11-12 08:45:17 +0100

    Correction Job18

--- jour08/job18/job18.sql
+++ jour08/job18/job18.sql
@@ -1,1 +1,1 @@
-SELECT etage.nom, salles.nom AS BiggestRoom, MAX(salles.capacite) FROM salles INNER JOIN etage ON salles.id_etage=etage.id
+SELECT etage.nom, salles.nom AS 'Biggest Room', MAX(salles.capacite) FROM salles INNER JOIN etage ON salles.id_etage=etage.id
